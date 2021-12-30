498b3442eb5aa4c3fdb5aa96fb60ce42e9b6c1be
Marcin Bobowicz <marcin.bobowicz@gmail.com>
2021-12-30 13:28:15 +0100

    day 7, first part

---
+++ day7a.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+	// "math"
+	"adventofcode2021/helper"
+)
+
+func main(){
+	data:= helper.ReadFileString("data7.txt")
+
+	tmp := strings.Split(data[0],",")
+
+	crabs := make([]int,0) 
+	mx:=0
+	for _,v:=range tmp{
+		i,_:= strconv.Atoi(v)
+		crabs = append(crabs,i)
+		if i>mx{mx=i}
+	}
+
+	prev := 999999999
+	for i:=0;i<mx;i++{
+		sum:=0
+		for _,v:=range crabs{
+			l:=v-i
+			if l<0{l*=-1}
+			sum+=l
+		}
+		if prev>sum{prev = sum}
+	}
+	fmt.Println(prev)
+
+}
